66e6a55d3a0993af73c7ceff5e82a94e8a532d3f
Jeremy <asirwaadj@gmail.com>
2022-05-25 02:35:52 +0530

    network established

--- lib/dashbarchat.dart
+++ lib/dashbarchat.dart
@@ -1,18 +1,31 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../CusDrawer.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart';
 
-class dashbarchat extends StatelessWidget {
-  const dashbarchat({
-    Key? key,
-  }) : super(key: key);
+class dashbarchat extends StatefulWidget {
+  const dashbarchat(this.wdata);
+
+  final List wdata;
+
+  @override
+  State<dashbarchat> createState() => _dashbarchatState();
+}
+
+class _dashbarchatState extends State<dashbarchat> {
+  @override
+  void initState() {
+    super.initState();
+    print(widget.wdata[0].runtimeType);
+  }
 
   @override
   Widget build(BuildContext context) {
     DateTime date = DateTime.now();
-    print(DateTime.daysPerWeek);
+    // print(DateTime.daysPerWeek);
     String dateFormat7 = DateFormat('EEEE').format(date);
     return BarChart(BarChartData(
         borderData: FlBorderData(show: false),
@@ -75,25 +88,25 @@ class dashbarchat extends StatelessWidget {
             )),
         barGroups: [
           BarChartGroupData(x: 1, barRods: [
-            BarChartRodData(toY: 7, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[0].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 2, barRods: [
-            BarChartRodData(toY: 10, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[1].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 3, barRods: [
-            BarChartRodData(toY: 2, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[2].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 4, barRods: [
-            BarChartRodData(toY: 5, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[3].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 5, barRods: [
-            BarChartRodData(toY: 9, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[4].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 6, barRods: [
-            BarChartRodData(toY: 3, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[5].toDouble(), width: 10, color: Colors.white),
           ]),
           BarChartGroupData(x: 7, barRods: [
-            BarChartRodData(toY: 8, width: 10, color: Colors.white),
+            BarChartRodData(toY: widget.wdata[6].toDouble(), width: 10, color: Colors.white),
           ]),
         ]));
   }
