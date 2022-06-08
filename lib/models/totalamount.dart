c9deaa784485e6adc39f5534e19b03c4387407d4
Jeremy <asirwaadj@gmail.com>
2022-06-09 02:07:50 +0530

    asdasd

---
+++ lib/models/totalamount.dart
@@ -0,0 +1,82 @@
+class totalamount {
+  Data? data;
+  Meta? meta;
+
+  totalamount({this.data, this.meta});
+
+  totalamount.fromJson(Map<String, dynamic> json) {
+    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+    meta = json['meta'] != null ? new Meta.fromJson(json['meta']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    if (this.meta != null) {
+      data['meta'] = this.meta!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? id;
+  Attributes? attributes;
+
+  Data({this.id, this.attributes});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    attributes = json['attributes'] != null
+        ? new Attributes.fromJson(json['attributes'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    if (this.attributes != null) {
+      data['attributes'] = this.attributes!.toJson();
+    }
+    return data;
+  }
+}
+
+class Attributes {
+  double? totalAmount;
+  String? createdAt;
+  String? updatedAt;
+  String? publishedAt;
+
+  Attributes(
+      {this.totalAmount, this.createdAt, this.updatedAt, this.publishedAt});
+
+  Attributes.fromJson(Map<String, dynamic> json) {
+    totalAmount = json['totalAmount'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    publishedAt = json['publishedAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['totalAmount'] = this.totalAmount;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    data['publishedAt'] = this.publishedAt;
+    return data;
+  }
+}
+
+class Meta {
+  Meta();
+
+  Meta.fromJson(Map<String, dynamic> json) {}
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    return data;
+  }
+}
